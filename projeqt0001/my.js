1d6e36173406bdc0021d7014ed322a6bb8f6bbeb
Lucas Rodrigues <lucasmainabegama27@yahoo.com>
2023-04-08 23:14:45 -0300

    alfa0005

--- projeqt0001/my.js
+++ projeqt0001/my.js
@@ -1,24 +1,22 @@
+
+//confing de menu_bar 
 function clickMenu(){
-    if(menu_bar.style.display=='block'){
-        menu_bar.style.display='none'
+    let open = window.document.getElementById('open_menu')
+    let menu = window.document.getElementById('menu_bar')
+    let close = window.document.getElementById('close')
+
+    if(menu.style.display=='block'){
+        menu.style.display='none'
+        close.style.display='none'
     }else{
-        menu_bar.style.display='block'
+        menu.style.display='block'
+        close.style.display='block'
+    }if(menu.style.display=='block'){
+           open.style.display='none'
+    }else{
+        open.style.display='block'
     }
-}
-let open = window.document.getElementById('open_menu')
-let close =window.document.getElementById('close')
-let menu =window.document.getElementById('menu_bar')
-open.addEventeListener('click',aberto)
-close.addEventeListener('click',fechado)
-
-function close(){
-    let fechado = document.getElementById('close')
+   
+    
 }
 
-function aberto(){
-    menu.style.display='none'
-}
-
-function fechado(){
-    menu.style.display='block'
-}
